529251533787e6d6701b6e84e004864c2e638bda
ZorroAEB <45982836+ZorroAEB@users.noreply.github.com>
2018-12-22 21:10:59 -0500

    Add files via upload

---
+++ face2.js
@@ -0,0 +1,11 @@
+rect(76, 45, 250, 300); // facial shape
+
+rect(126, 250, 152, 60); // mouth
+
+rect(140, 150, 30, 30); // left eye
+
+rect(239, 150, 30, 30); // right eye
+
+rect(175, 345, 50, 50); // neck
+
+unibrow(140, 150, 270, 129); // unibrow
