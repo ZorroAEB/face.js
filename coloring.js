a362e631490827507a52d9aa9707e3cdecef5cae
ZorroAEB <45982836+ZorroAEB@users.noreply.github.com>
2018-12-23 13:12:02 -0500

    Add files via upload

---
+++ coloring.js
@@ -0,0 +1,23 @@
+background(148, 251, 255); // RGB Colors
+
+noStroke(); // no outlines
+
+fill(165, 42, 42) // brown
+
+ellipse(202, 208, 300, 300); // face
+
+fill(0, 0, 0) // black
+
+ellipse(157, 151, 40, 40); // left eye
+
+ellipse(304, 142, 40, 40); //right eye
+
+fill(255, 0, 0) // red
+
+ellipse(257, 240, 120, 136); // mouth
+
+strokeWeight(33); // headband width
+
+stroke(250, 0, 242); // way to color lines and shape outlines
+
+line(83, 116, 271, 74); // headband
