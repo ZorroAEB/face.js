92288c4dcfc0b7704739d051319abede8f11b328
ZorroAEB <45982836+ZorroAEB@users.noreply.github.com>
2018-12-24 13:44:41 -0500

    Add files via upload

---
+++ sunny_snowy_day.js
@@ -0,0 +1,12 @@
+background(0, 255, 255);
+// grass
+fill(0, 255, 68); 
+rect(0, 300, 400, 100);  
+// the sun
+fill(209, 209, 20);
+ellipse(80, 64, 100, 100); 
+// the snowman
+fill(245, 242, 242);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
